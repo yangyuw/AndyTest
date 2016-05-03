2f22b7d70e021f2b91278b24f041fa828bc75f32
andy <youngyuhpe@gmail.com>
2016-05-03 22:48:22 +0800

    3

--- app/src/main/java/com/andy/andytest/CrimeActivity.java
+++ app/src/main/java/com/andy/andytest/CrimeActivity.java
@@ -1,8 +1,9 @@
 package com.andy.andytest;
 
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
 
 public class CrimeActivity extends FragmentActivity {
 
@@ -10,5 +11,12 @@ public class CrimeActivity extends FragmentActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crime);
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
+
+        if(fragment == null) {
+            fragment = new CrimeFragment();
+            fm.beginTransaction().add(R.id.fragment_container, fragment).commit();
+        }
     }
 }
